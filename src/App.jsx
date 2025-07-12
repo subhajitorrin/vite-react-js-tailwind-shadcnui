c43ec3e1847304a2d2d46599d0a51c4a8d31179b
orrin-buildmvpfa.st <subhajitghosh@buildmvpfa.st>
2025-07-12 21:41:02 +0530

    template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div className='border bg-red-100 p-4 space-y-4'>
+      <Button >Click me</Button>
+    </div>
+  )
+}
+
+export default App
